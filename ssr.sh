4496bf3242f110ff0ffd90c5c57f2c42aa440082
Sao！ <o_u1000@dragons.hutchcc.edu>
2017-03-05 20:21:11 +0800

    Add files via upload

--- ssr.sh
+++ ssr.sh
@@ -9,16 +9,6 @@ export PATH
 #   Power for LIBERTY-SS                                          #
 #=================================================================#
 
-
-
-#Current folder
-cur_dir=`pwd`
-# Get public IP address
-IP=$(ip addr | egrep -o '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | egrep -v "^192\.168|^172\.1[6-9]\.|^172\.2[0-9]\.|^172\.3[0-2]\.|^10\.|^127\.|^255\.|^0\." | head -n 1)
-if [[ "$IP" = "" ]]; then
-    IP=$(wget -qO- -t1 -T2 ipv4.icanhazip.com)
-fi
-
 # Make sure only root can run our script
 function rootness(){
     if [[ $EUID -ne 0 ]]; then
@@ -31,15 +21,11 @@ function rootness(){
 function checkos(){
     if [ -f /etc/redhat-release ];then
         OS='CentOS'
-    elif [ ! -z "`cat /etc/issue | grep bian`" ];then
-        OS='Debian'
-    elif [ ! -z "`cat /etc/issue | grep Ubuntu`" ];then
-        OS='Ubuntu'
-    else
         echo "Not support OS, Please reinstall OS and retry!"
         exit 1
     fi
 }
+
 
 # Get version
 function getversion(){
@@ -70,54 +56,61 @@ if [ -s /etc/selinux/config ] && grep 'SELINUX=enforcing' /etc/selinux/config; t
 fi
 }
 
-# Pre-installation settings
-function pre_install(){
-    # Not support CentOS 5
-    if centosversion 5; then
-        echo "Not support CentOS 5, please change OS to CentOS 6+/Debian 7+/Ubuntu 12+ and retry."
-        exit 1
-    fi
-   
-   
+
 # Download files
 function download_files(){
     # Download libsodium file
 	if ! wget --no-check-certificate -O libsodium-1.0.10.tar.gz https://github.com/jedisct1/libsodium/releases/download/1.0.10/libsodium-1.0.10.tar.gz; then
         echo "Failed to download libsodium file!"
         exit 1
-	fi
-    # Download ShadowsocksR file
+    fi
+	# Clone Shadowsocks file
 	if [ "$OS" == 'CentOS' ]; then
-        if ! wget --no-check-certificate -O shadowsocks git clone -b manyuser https://github.com/glzjin/shadowsocks.git; then
-            echo "Failed to download Shadowsocks chkconfig file!"
+	    if ! git clone -b manyuser https://github.com/glzjin/shadowsocks.git; then
+	        echo "Failed to download ShadowsocksR chkconfig file!"
             exit 1
-		fi
+        fi
 	fi
 }
-		
-    # Install necessary dependencies
+
+# Install necessary dependencies
+function pre_install(){
     if [ "$OS" == 'CentOS' ]; then
-	    apt-get -y update
-	    yum install python-setuptools && easy_install pip
-		yum install git
-		yum -y groupinstall "Development Tools"
-		tar xf libsodium-1.0.10.tar.gz && cd libsodium-1.0.10
-		./configure && make -j2 && make install
-		echo /usr/local/lib > /etc/ld.so.conf.d/usr_local_lib.conf
-		ldconfig
+        yum install python-setuptools && easy_install pip
+	    yum install git
+	    yum -y groupinstall "Development Tools"
+	    tar xf libsodium-1.0.10.tar.gz && cd libsodium-1.0.10
+	    ./configure && make -j2 && make install
+	    echo /usr/local/lib > /etc/ld.so.conf.d/usr_local_lib.conf
+	    ldconfig
+		yum -y install python-devel
+        yum -y install libffi-devel
+        yum -y install openssl-devel
+    fi
 	cd $cur_dir
 }
-# Installation dependent
-     if [ "$OS" == 'CentOS' ]; then
-		cd shadowsocks
-		yum -y install python-devel
-		yum -y install libffi-devel
-		yum -y install openssl-devel
-		pip install -r requirements.txt
+
+# config
+function pre_install(){
+    if [ "$OS" == 'CentOS' ]; then
+	    cd shadowsocks
 		cp apiconfig.py userapiconfig.py
-		cp config.json user-config.json
-     fi
+        cp apiconfig.py userapiconfig.py
+        cp config.json user-config.json
+		pip install -r requirements.txt
+	fi
+    cd $cur_dir
 }
 
-# optimization
 
+	
+
+
+
+
+	
+
+
+
+	
+	
