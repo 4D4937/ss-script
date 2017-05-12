a4bedb2fbc69cd7834d39b45c0afcf773921356f
T3ns0r <o_u1000@dragons.hutchcc.edu>
2017-05-12 19:24:48 +0800

    Add files via upload

--- ssr2.sh
+++ ssr2.sh
@@ -10,9 +10,7 @@ echo "#############################################################"
 echo
 #Check Root
 [ $(id -u) != "0" ] && { echo "Error: You must be root to run this script"; exit 1; }
-read -p "Please input your domain(like:https://ss.feiyang.li or http://114.114.114.114): " Userdomain
-read -p "Please input your mukey(like:mupass): " Usermukey
-read -p "Please input your Node_ID(like:1): " UserNODE_ID
+
 #check OS version
 check_sys(){
 	if [[ -f /etc/redhat-release ]]; then
@@ -37,10 +35,10 @@ install_soft_for_each(){
 	if [[ ${release} = "centos" ]]; then
 		echo "Will install below software on your centos system:"
 		yum install git -y
-		yum install python-setuptools -y 
+		yum install python-setuptools && easy_install pip -y
 		yum -y groupinstall "Development Tools" -y
-		wget https://raw.githubusercontent.com/mmmwhy/ss-panel-and-ss-py-mu/master/libsodium-1.0.11.tar.gz
-		tar xf libsodium-1.0.11.tar.gz && cd libsodium-1.0.11
+		wget https://github.com/jedisct1/libsodium/releases/download/1.0.10/libsodium-1.0.10.tar.gz
+		tar xf libsodium-1.0.11.tar.gz && cd libsodium-1.0.10
 		./configure && make -j2 && make install
 		echo /usr/local/lib > /etc/ld.so.conf.d/usr_local_lib.conf
 		ldconfig
@@ -58,22 +56,20 @@ install_soft_for_each(){
 	fi
 }
 
-install_soft_for_each
-echo "Let's setup your ssnode/root"
-git clone https://github.com/mmmwhy/shadowsocks-py-mu.git "/root/shadowsocks-py-mu"
-#modify Config.py
-echo -e "modify Config.py...\n"
-Userdomain=${Userdomain:-"https://ss.feiyang.li"}
-sed -i "s#http://domain#${Userdomain}#" /root/shadowsocks-py-mu/shadowsocks/config.py
-Usermukey=${Usermukey:-"mupass"}
-sed -i "s#mupass#${Usermukey}#" /root/shadowsocks-py-mu/shadowsocks/config.py
-UserNODE_ID=${UserNODE_ID:-"1"}
-sed -i "s#'1'#'${UserNODE_ID}'#" /root/shadowsocks-py-mu/shadowsocks/config.py
-echo_supervisord_conf > /etc/supervisord.conf
-sed -i '$a [program:ss-manyuser]\ncommand = python /root/shadowsocks-py-mu/shadowsocks/servers.py\nuser = root\nautostart = true\nautorestart = true' /etc/supervisord.conf
-supervisord
+#clone shadowsocks
+cd /root
+git clone -b manyuser https://github.com/glzjin/shadowsocks.git "/root/shadowsocks"
+#install devel
+cd /root/shadowsocks
+yum -y install python-devel
+yum -y install libffi-devel
+yum -y install openssl-devel
+pip install -r requirements.txt
+cp apiconfig.py userapiconfig.py
+cp config.json user-config.json
+
+#iptables
 iptables -I INPUT -p tcp -m tcp --dport 104 -j ACCEPT
 iptables -I INPUT -p tcp -m tcp --dport 1024: -j ACCEPT
 iptables-save
-sleep 4
-cat shadowsocks.log
+
