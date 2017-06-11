644049f7392f9582e208d6b022f682cdd0b9faf7
T3ns0r <o_u1000@dragons.hutchcc.edu>
2017-06-11 20:16:38 +0800

    ssr_db.sh
    支持sql配置

--- SSR-Python.sh
+++ SSR-Python.sh
@@ -3,9 +3,11 @@ clear
 ########################################################
 # System Required: CentOS 6+                           #
 # Description: One click Install ShadowsocksR-Python   #
-# Version: 1.1.0                                       #
+# Version: 1.2                                         #
 # Author: T3ns0r                                       #
 ########################################################
+
+config_file="/root/shadowsocks/userapiconfig.py"
 
 #Check Root
 [ $(id -u) != "0" ] && { echo "Error: You must be root to run this script"; exit 1; }
@@ -29,6 +31,37 @@ check_sys(){
     fi
 	bit=`uname -m`
 }
+
+Add_iptables(){
+	iptables -I INPUT -m state --state NEW -m tcp -p tcp --dport 1:65535 -j ACCEPT
+	iptables -I INPUT -m state --state NEW -m udp -p udp --dport 1:65535 -j ACCEPT
+}
+
+Save_iptables(){
+	if [[ ${release} == "centos" ]]; then
+		service iptables save
+	else
+		iptables-save > /etc/iptables.up.rules
+	fi
+}
+
+# config
+read -p "节点ID:" id_name
+read -p "数据库地址:" sql_host
+read -p "数据库:" sql_db
+read -p "用户名:" sql_user
+read -p "数据库密码:" sql_pw
+
+View_User(){
+        if [[ ${release} = "centos" ]]; then
+		        echo -e " 数据库 配置信息：" && echo
+                echo -e " 地址\t    : ${Green_font_prefix}${sql_host}${Font_color_suffix}"
+				echo -e " 用户\t    : ${Green_font_prefix}${sql_user}${Font_color_suffix}"
+				echo -e " 密码\t    : ${Green_font_prefix}${sql_pw}${Font_color_suffix}"
+				echo -e " 数据库\t    : ${Green_font_prefix}${sql_db}${Font_color_suffix}"
+		fi
+}		
+				
 install_soft_for_each(){
 	    check_sys
 	    if [[ ${release} = "centos" ]]; then
@@ -65,9 +98,12 @@ pip install -r requirements.txt
 cp apiconfig.py userapiconfig.py
 cp config.json user-config.json
 
-#iptables
-iptables -I INPUT -p tcp -m tcp --dport 1 -j ACCEPT
-iptables -I INPUT -p tcp -m tcp --dport 65535: -j ACCEPT
-iptables-save
+sed -i "2s/1/${id_name}/g" ${config_file}
+sed -i "24s/127.0.0.1/${sql_host}/g" ${config_file}
+sed -i "26s/ss/${sql_user}/g" ${config_file}
+sed -i "27s/ss/${sql_pw}/g" ${config_file}
+sed -i "28s/shadowsocks/${sql_db}/g" ${config_file}
+
+
 
 echo enjoy it!
